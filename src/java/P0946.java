259bbc0670a5d98009a718989630ebc03e9c77ff
Alicia-Sheng <duowensheng@gmail.com>
2020-07-24 01:06:10 -0400

    Update solutions

---
+++ src/java/P0946.java
@@ -0,0 +1,21 @@
+//946. Validate Stack Sequences
+
+package java;
+
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        int N = pushed.length;
+        Stack<Integer> stack = new Stack();
+
+        int j = 0;
+        for (int x: pushed) {
+            stack.push(x);
+            while (!stack.isEmpty() && j < N && stack.peek() == popped[j]) {
+                stack.pop();
+                j++;
+            }
+        }
+
+        return j == N;
+    }
+}
