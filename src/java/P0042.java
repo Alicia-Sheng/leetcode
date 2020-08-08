071f895ef0189737fa960c2678f0b474a19d2ed5
Alicia-Sheng <duowensheng@gmail.com>
2020-08-08 18:03:37 -0400

    Finish P0053

--- src/java/P0042.java
+++ src/java/P0042.java
@@ -8,17 +8,18 @@ class P0042 {
             return 0;
         }
         int res = 0;
-        int[] left = new int[height.length];
+        int length = height.length;
+        int[] left = new int[length];
         left[0] = height[0];
-        int[] right = new int[height.length];
-        right[height.length - 1] = height[height.length - 1];
-        for (int i = 1; i < height.length; i++) {
+        int[] right = new int[length];
+        right[length - 1] = height[length - 1];
+        for (int i = 1; i < length; i++) {
             left[i] = Math.max(height[i], left[i - 1]);
         }
-        for (int j = height.length - 2; j > -1; j--) {
-            right[j] = Math.max(height[j], right[j + 1]);
+        for (int i = length - 2; i > -1; i--) {
+            right[i] = Math.max(height[i], right[i + 1]);
         }
-        for (int i = 0; i < height.length; i++) {
+        for (int i = 0; i < length; i++) {
             res += Math.min(left[i], right[i]) - height[i];
         }
         return res;
---
+++ src/java/P0053.java
@@ -0,0 +1,33 @@
+//53. Maximum Subarray
+package java;
+
+class P0053 {
+    public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int res = nums[0];
+        int[] max = new int[nums.length];
+        max[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            max[i] = Math.max(nums[i], (nums[i] + max[i - 1]));
+            res = max[i] > res ? max[i] : res;
+        }
+        return res;
+        // if (nums == null || nums.length == 0) {
+        //     return 0;
+        // }
+        // int res = nums[0];
+        // int temp = 0;
+        // for (int i = 0; i < nums.length; i++) {
+        //     for (int j = i; j < nums.length; j++) {
+        //         temp += nums[j];
+        //         if (temp > res) {
+        //             res = temp;
+        //         }
+        //     }
+        //     temp = 0;
+        // }
+        // return res;
+    }
+}
