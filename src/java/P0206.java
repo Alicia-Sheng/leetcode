34ec9cae436eac3caa3c7458e17e2724fb824a5e
Alicia-Sheng <duowensheng@gmail.com>
2020-08-22 18:08:25 -0400

    Finish P0206

---
+++ src/java/P0206.java
@@ -0,0 +1,27 @@
+//206. Reverse Linked List
+
+package java;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class P0206 {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
